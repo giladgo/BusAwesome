cb48e9805667693b7d06243c8de95e7ea1db97d6
Gilad Goldberg <giladgo@gmail.com>
2013-10-30 11:36:13 +0200

    Adding trip display screen

---
+++ BusAwesome/Controllers/Trip/BUSTripVC.h
@@ -0,0 +1,13 @@
+//
+//  BUSTripVC.h
+//  BusAwesome
+//
+//  Created by Gilad Goldberg on 10/30/13.
+//  Copyright (c) 2013 Gilad Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BUSTripVC : UIViewController
+
+@end
---
+++ BusAwesome/Controllers/Trip/BUSTripVC.m
@@ -0,0 +1,38 @@
+//
+//  BUSTripVC.m
+//  BusAwesome
+//
+//  Created by Gilad Goldberg on 10/30/13.
+//  Copyright (c) 2013 Gilad Goldberg. All rights reserved.
+//
+
+#import "BUSTripVC.h"
+
+@interface BUSTripVC ()
+
+@end
+
+@implementation BUSTripVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
