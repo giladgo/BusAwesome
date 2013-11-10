b0d8fb8473ff72cdc1a9d4949a3151817d409d31
Gilad Goldberg <giladgo@gmail.com>
2013-11-10 14:08:37 +0200

    Commenting the highlight enum

--- BusAwesome/Views/Trip/BUSStopWidget.h
+++ BusAwesome/Views/Trip/BUSStopWidget.h
@@ -8,11 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+// An enum which defined how each stop in the list can be highlighted or dimmed (if visited)
 typedef enum StopHighlightMode : NSUInteger {
+  // Nothing special
   StopHighlightModeNone,
+  // Highlight just the stop's circle
   StopHighlightModeStop,
+  // Highlight the stop's circle and the line leading to it from the top
   StopHighlightModeStopAndTop,
+  // Highlight the stop's circle and the line leading to it from the bottom
   StopHighlightModeStopAndBottom,
+  // Don't highlight; instead, dim it to make it appear visited
   StopHighlightModeVisited
 } StopHighlightMode;
 
